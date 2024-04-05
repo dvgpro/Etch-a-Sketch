b9b11ac9f06dccc7d254628825cd3310c0f69f7f
dgin <128121988+dvgpro@users.noreply.github.com>
2024-04-05 17:33:20 +0900

    Add functions to show and hide the modal
    Add two functions that show or hide the setting modal when buttons are
    clicked.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const modal = document.querySelector('.settings-modal');
 const closeButton = document.querySelector('.close-modal');
 const openButton = document.querySelector('.setting-btn');
 const reloadButton = document.querySelector('.reset-btn');
+
+openButton.addEventListener('click', ()=>
+{
+    modal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () =>
+{
+    modal.style.display = 'none';
+});
