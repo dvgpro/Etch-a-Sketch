3b5b01324da8c967ea46dd9b3e5cef4c19451284
dgin <128121988+dvgpro@users.noreply.github.com>
2024-04-05 19:15:31 +0900

    Add function to build grid
    Add function to build grid inside container based on given number
    of boxes per row. Function also gives each box attributes to changes
    color when the mouse goes over them.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const closeButton = document.querySelector('.close-modal');
 const openButton = document.querySelector('.setting-btn');
 const reloadButton = document.querySelector('.reset-btn');
 
+const grid = document.querySelector('.grid-container');
+
+
+
 openButton.addEventListener('click', ()=>
 {
     modal.style.display = 'block';
@@ -17,3 +21,31 @@ reloadButton.addEventListener('click', () =>
 {
     location.reload();
 });
+
+let boxesRow = 16;
+let containerSize = 720;
+
+let boxColor = 'black';
+
+function buildGrid()
+{
+    for(let i=0; i<(boxesRow**2);i++)
+    {
+        let box = document.createElement('div');
+        box.classList.add('squares');
+        box.setAttribute(
+            'style', 
+            `width: ${containerSize/boxesRow}px;
+            height: ${containerSize/boxesRow}px;`
+        );
+
+        box.addEventListener('mouseenter', () =>
+        {
+            box.style.backgroundColor = boxColor;
+        });
+
+        grid.appendChild(box);
+    };
+};
+
+buildGrid();
