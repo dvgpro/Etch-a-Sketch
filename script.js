a948b65951d0e212a40fcda1f105501ab5b7abc9
dgin <128121988+dvgpro@users.noreply.github.com>
2024-04-05 17:15:20 +0900

    Add constants for the modal
    Add constants to be used to enable and disabled the modal display
    functionality.

---
+++ script.js
@@ -0,0 +1,4 @@
+const modal = document.querySelector('.settings-modal');
+const closeButton = document.querySelector('.close-modal');
+const openButton = document.querySelector('.setting-btn');
+const reloadButton = document.querySelector('.reset-btn');
