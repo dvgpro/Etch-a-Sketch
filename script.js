b6310eac3b3cd9775d6854e31fb8f7ee7c693798
dgin <128121988+dvgpro@users.noreply.github.com>
2024-04-06 16:21:45 +0900

    Add files for the game to work

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const modal = document.querySelector('.settings-modal');
-const closeButton = document.querySelector('.close-modal');
-const openButton = document.querySelector('.setting-btn');
 const reloadButton = document.querySelector('.reset-btn');
-
 const grid = document.querySelector('.grid-container');
+const clearGrid = document.querySelector('.clear-btn');
 
+const modal = document.querySelector('.settings-modal');
+const modalShow = document.querySelector('.setting-btn');
+const modalHide = document.querySelector('.closeModalBtn');
 
+const rainbowColors = document.querySelector('.rainbowBtn');
+const applyColor = document.querySelector('.applyColorBtn');
+const givenColor = document.querySelector('.inputColorBox');
+const applyRow = document.querySelector('.applySizeChange');
+const givenRowSize = document.querySelector('.inputRowBox');
 
-openButton.addEventListener('click', ()=>
+modalShow.addEventListener('click', () =>
 {
     modal.style.display = 'block';
 });
 
-closeButton.addEventListener('click', () =>
+modalHide.addEventListener('click', () => 
 {
     modal.style.display = 'none';
 });
@@ -22,30 +27,81 @@ reloadButton.addEventListener('click', () =>
     location.reload();
 });
 
-let boxesRow = 16;
+let boxesPerRow = 16;
 let containerSize = 720;
-
 let boxColor = 'black';
+let clicked = 'false';
 
-function buildGrid()
+function buildGrid(row, colors, clicked)
 {
-    for(let i=0; i<(boxesRow**2);i++)
+    for(let i=0; i<(row**2);i++)
     {
         let box = document.createElement('div');
         box.classList.add('squares');
         box.setAttribute(
             'style', 
-            `width: ${containerSize/boxesRow}px;
-            height: ${containerSize/boxesRow}px;`
+            `width: ${containerSize/row}px;
+            height: ${containerSize/row}px;`
         );
 
         box.addEventListener('mouseenter', () =>
         {
-            box.style.backgroundColor = boxColor;
+            if (clicked === 'true')
+            {
+                if (box.style.backgroundColor == '')
+                {
+                    var letters = '0123456789ABCDEF';
+                    colors = '#';
+
+                    for (var x = 0; x < 6; x++) 
+                    {
+                        colors += letters[Math.floor(Math.random() * 16)];
+                    };
+                    box.style.backgroundColor = colors;
+                };
+            };
+
+            if (box.style.backgroundColor == '')
+            {
+                box.style.backgroundColor = colors;
+            };
+
+            if (box.style.opacity <= 1)
+            {
+                box.style.opacity = +box.style.opacity + 0.1;
+            };
         });
 
         grid.appendChild(box);
     };
 };
 
-buildGrid();
+buildGrid(boxesPerRow, boxColor);
+
+rainbowColors.addEventListener('click', () =>
+{
+    grid.innerHTML = '';
+    clicked= 'true';
+    buildGrid(boxesPerRow, boxColor, clicked);
+});
+
+clearGrid.addEventListener('click', () =>
+{
+    grid.innerHTML = '';
+    buildGrid(boxesPerRow, boxColor, clicked);
+});
+
+applyColor.addEventListener('click', () =>
+{
+    grid.innerHTML = '';
+    boxColor = givenColor.textContent;
+    clicked = 'false';
+    buildGrid(boxesPerRow, boxColor, clicked);
+});
+
+applyRow.addEventListener('click', () => 
+{
+    grid.innerHTML = '';
+    boxesPerRow = parseInt(givenRowSize.textContent);
+    buildGrid(boxesPerRow, boxColor, clicked);
+});
